cfef261fb8930aa1ce3a02fc36491f27dcc4f382
TxMat <Mathieu.ponal@gmail.com>
2024-11-07 11:21:35 +0100

    fix avatar + better logging + gitlab result first if any

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
 
 app = FastAPI()
 
@@ -15,6 +16,7 @@ async def read_index():
 # Your existing search endpoint
 @app.get("/search")
 async def search_users(query: str):
+    logger.info(f"Searching for '{query}'")
     import httpx
     GITHUB_API_URL = "https://api.github.com/search/users"
     GITLAB_API_URL = "https://gitlab.com/api/v4/users"
@@ -25,16 +27,25 @@ async def search_users(query: str):
 
         gitlab_response = await client.get(GITLAB_API_URL, params={"username": query})
         gitlab_users = gitlab_response.json()
-        
-    print(github_users)
-    print(gitlab_users)
-    
-    for user in gitlab_users:
-        print(user)
-    
+
+    if len(github_users) == 0 and len(gitlab_users) == 0:
+        logger.error("No users found")
+    else:
+        logger.info(f"Found: {len(github_users) + len(gitlab_users)} Users")
+
+    if len(github_users) > 0:
+        logger.info(f"{len(github_users)} from GitHub")
+    else:
+        logger.warning("No Users on GitHub")
+
+    if len(gitlab_users) > 0:
+        logger.info(f"{len(gitlab_users)} from GitLab")
+    else:
+        logger.warning("No Users on GitLab")
+
     users = {
-       "github": [{"username": user["login"], "url": user["html_url"]} for user in github_users],
-       "gitlab": [{"username": user["username"], "url": user["web_url"]} for user in gitlab_users],
+       "github": [{"username": user["login"], "url": user["html_url"], "avatar": user["avatar_url"] } for user in github_users],
+       "gitlab": [{"username": user["username"], "url": user["web_url"], "avatar": user["avatar_url"]} for user in gitlab_users],
     }
     return users
 
